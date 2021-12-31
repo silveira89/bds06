c0fa0ef60770f5e4711ad83bc5c77df9ff3de74f
Andre Silveira <alasandre@gmail.com>
2021-12-31 11:44:00 -0300

    MovieDTO, MovieMinDTO, MovieService, MovieController and fix: MovieRepository

---
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.MovieMinDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class MovieService{
+	
+	@Autowired
+	private MovieRepository repository;
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Optional<Movie> obj = repository.findById(id);
+		Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+		return new MovieDTO(entity, entity.getGenre());
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<MovieMinDTO> findByGenrePaged(Long genreId, Pageable pageable) {
+		genreId = (genreId == 0) ? null : genreId;
+		Page<MovieMinDTO> page = repository.findByGenrePaged(genreId, pageable);
+		return page;
+	}
+	
+}
